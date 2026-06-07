359d1254131b689732a91068289b75ab5e3963dd
Valeria <vestrada@centro.edu.mx>
2026-06-07 17:17:23 -0600

    Changed_graphics_yay_also_changed_button_for_popup_made_it_toggleable_also_changed_woff2_for_font

--- juego/js/popup.js
+++ juego/js/popup.js
@@ -1,8 +1,4 @@
 function showPopupInfo() {
     var popup = document.getElementById("myPopupInfo");
-    popup.classList.add("show");
-}
-function closePopupInfo() {
-    var popup = document.getElementById("myPopupInfo");
-    popup.classList.remove("show");
+    popup.classList.toggle("show");
 }
